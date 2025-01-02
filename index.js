eef66d6d612be1e750449f49f9af8c599d7d136e
skylar3 <skylartipton01@yahoo.com>
2025-01-02 14:42:37 -0500

    added it so the squares change color when mouse hovers over

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const gridSquareContainer = document.querySelector("#gridSquareContainer");
 for (let i = 0; i <16; i++)
 {
         const square = document.createElement("div");
+        square.addEventListener("mouseover", () =>
+        {
+            square.style.backgroundColor = "blue";
+
+        });
         square.classList.add("square");
         gridSquareContainer.appendChild(square);
 }
